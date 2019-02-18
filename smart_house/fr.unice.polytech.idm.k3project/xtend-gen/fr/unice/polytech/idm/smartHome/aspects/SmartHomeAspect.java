e01d1a48fcf8f9cb4317037b36962850f0886b83
Johann Mortara <johann.mortara@etu.unice.fr>
2019-02-18 17:03:54 +0100

    last modifications

--- smart_house/fr.unice.polytech.idm.k3project/xtend-gen/fr/unice/polytech/idm/smartHome/aspects/SmartHomeAspect.java
+++ smart_house/fr.unice.polytech.idm.k3project/xtend-gen/fr/unice/polytech/idm/smartHome/aspects/SmartHomeAspect.java
@@ -89,8 +89,8 @@ public class SmartHomeAspect {
         EList<Rule> _rules = _self.getRules();
         for (final Rule rule : _rules) {
           String _description = rule.getEvent().getDescription();
-          String _plus_2 = ("Rule " + _description);
-          String _plus_3 = (_plus_2 + " value : ");
+          String _plus_2 = ("Rule \"" + _description);
+          String _plus_3 = (_plus_2 + "\" value : ");
           boolean _evaluateRule = RuleAspect.evaluateRule(rule);
           String _plus_4 = (_plus_3 + Boolean.valueOf(_evaluateRule));
           InputOutput.<String>println(_plus_4);
